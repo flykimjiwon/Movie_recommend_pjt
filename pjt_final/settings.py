4ce0a507bf41c46bbe879f7adaeb8b20189feb9c
김지원 <flykimjiwun@naver.com>
2021-11-24 09:16:47 +0900

    1124오전readme작성

--- pjt_final/settings.py
+++ pjt_final/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # STATIC_ROOT = BASE_DIR / 'staticfiles'
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static',
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # 이게문제였군
 
 
